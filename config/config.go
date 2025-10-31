b6fd8c0cebbf700bbbd9dd899add850e43d5e221
David Pareja Arango <davidpaar14@gmail.com>
2025-10-30 22:44:06 -0500

    chore: uncomment the code to retrive the EXCHANGE_RATE_API_KEY from parameter store

--- config/config.go
+++ config/config.go
@@ -2,12 +2,16 @@ package config
 
 import (
 	// "context"
+	"context"
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	// "github.com/aws/aws-sdk-go-v2/aws"
-    // "github.com/aws/aws-sdk-go-v2/config"
-    // "github.com/aws/aws-sdk-go-v2/service/ssm"
+	// "github.com/aws/aws-sdk-go-v2/config"
+	// "github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 const (
@@ -20,30 +24,31 @@ type Config struct {
 }
 
 func LoadConfig() (*Config, error) {
-	exchangeKey := os.Getenv("EXCHANGE_RATE_API_KEY")
-	if exchangeKey == "" {
-		return &Config{}, fmt.Errorf("EXCHANGE_RATE_API_KEY is not set")
-	}
+	// Get from local
+	// exchangeKey := os.Getenv("EXCHANGE_RATE_API_KEY")
+	// if exchangeKey == "" {
+	// 	return &Config{}, fmt.Errorf("EXCHANGE_RATE_API_KEY is not set")
+	// }
 
-	// Fetching secrets directly from AWS Parameter Store
-	// ctx := context.TODO()
+	//Fetching secrets directly from AWS Parameter Store
+	ctx := context.TODO()
 
-    // // Load AWS default configuration (uses EC2 IAM role automatically)
-    // cfg, err := config.LoadDefaultConfig(ctx)
-    // if err != nil {
-    //     return &Config{}, fmt.Errorf("failed to load AWS config: %w", err)
-    // }
+    // Load AWS default configuration (uses EC2 IAM role automatically)
+    cfg, err := config.LoadDefaultConfig(ctx)
+    if err != nil {
+        return &Config{}, fmt.Errorf("failed to load AWS config: %w", err)
+    }
 
-    // client := ssm.NewFromConfig(cfg)
-    // param, err := client.GetParameter(ctx, &ssm.GetParameterInput{
-    //     Name:           aws.String(name),
-    //     WithDecryption: aws.Bool(true),
-    // })
-    // if err != nil {
-    //     return &Config{}, fmt.Errorf("EXCHANGE_RATE_API_KEY is not set, Error: %w", err)
-    // }
+    client := ssm.NewFromConfig(cfg)
+    param, err := client.GetParameter(ctx, &ssm.GetParameterInput{
+        Name:           aws.String(name),
+        WithDecryption: aws.Bool(true),
+    })
+    if err != nil {
+        return &Config{}, fmt.Errorf("EXCHANGE_RATE_API_KEY is not set, Error: %w", err)
+    }
 
-    // exchangeKey := *param.Parameter.Value
+    exchangeKey := *param.Parameter.Value
 
 	// Fetching secrets from .env file
 	exchangeRatesKey := os.Getenv("EXCHANGE_RATES_API_KEY")
